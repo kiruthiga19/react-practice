d43a06c3bd8a0ea396c999de75c6e10c6d5114f8
kiruthiga19 <kiruthigapadmavathi@gmail.com>
2022-09-15 16:22:26 +0530

    fetching data from api

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -11,15 +11,15 @@ function DataFetching() {
         setIdFromButtonClick(id)
     }
     useEffect(()=>{
-        // axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
-        .then(res =>{
-            console.log(res)
-            setPost(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    },[idFromButtonClick])
+        //  axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
+        // .then(res =>{
+        //     console.log(res)
+        //     setPost(res.data)
+        // })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+    // },[idFromButtonClick])
   return (
     <div>
         <input type="text" value ={id}onChange={e => setId(e.target.value)}></input>
