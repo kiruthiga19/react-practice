09303f6d9ef2cb62bc5a976e3b3e9e6e13f3c5b4
kiruthiga19 <kiruthigapadmavathi@gmail.com>
2022-09-19 16:48:08 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './component/Greet';
+import Welcome from './component/Welcome';
+import Hello from './component/Hello';
+import Props from './component/Props';
+import Message from './component/Message';
+import Counter from './component/Counter';
+import FunctionClick from './component/functionClick';
+import ClassClick from './component/ClassClick';
+import EventBind from './component/EventBind';
+import ParentComponent from './component/ParentComponent';
+import UserGreeting from './component/UserGreeting';
+import NameList from './component/NameList';
+import IndexAsKey from './component/IndexAsKey';
+import StyleSheet from './component/StyleSheet';
+import Inline from './component/inline';
+import './appStyles.css'
+import styles from './appStyle.module.css'
+import Form from './component/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form></Form>
+      {/* <h1 className='error'>error</h1>
+      <h1 className={styles.sucess}>sucess</h1> */}
+      {/* <Inline></Inline> */}
+      {/* <StyleSheet primary ={true}></StyleSheet> */}
+      {/* <NameList></NameList> */}
+      {/* <IndexAsKey></IndexAsKey> */}
+      {/* <UserGreeting></UserGreeting> */}
+      {/* <ParentComponent></ParentComponent> */}
+      {/* <EventBind></EventBind> */}
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick></ClassClick> */}
+      {/* <Counter></Counter> */}
+      {/* <Message>
+      </Message> */}
+     {/* <Greet></Greet> */}
+     {/* <Welcome name="bruce" heroName="batman"></Welcome>
+     <Welcome  name="clark" heroName="batman"></Welcome>
+     <Welcome  name="bruce" heroName="batman"></Welcome> */}
+     {/* <Hello></Hello> */}
+     {/* <Props name="bruce" heroName="batman">
+      <p>this is chldren props</p>
+     </Props>
+     <Props name="clark" heroName="batman">
+      <button>submit</button>
+     </Props>
+     <Props name="diana" heroName="batman"></Props> */}
     </div>
   );
 }
